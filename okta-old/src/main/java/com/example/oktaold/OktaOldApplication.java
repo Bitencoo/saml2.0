c572a52de84a534b38453cb59f94f4e8491ffb86
Túlio Bitencourt <81566955+Bitencoo@users.noreply.github.com>
2023-04-26 16:22:41 -0300

    Add files via upload

---
+++ okta-old/src/main/java/com/example/oktaold/OktaOldApplication.java
@@ -0,0 +1,13 @@
+package com.example.oktaold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaOldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaOldApplication.class, args);
+	}
+
+}
---
+++ okta-old/src/test/java/com/example/oktaold/OktaOldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oktaold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaOldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
